a8b1b2cbb3d3ec5a13a037e961c56d38b1e0e5cb
AbdullahShokr1 <darkthe254@gmail.com>
2025-08-30 23:21:07 +0300

    Learn Module 1

--- src/product.jsx
+++ src/product.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Product = () => {
-    return <h1>My first Product</h1>;
+class Product extends Component {
+    state={
+        name:"burger",
+        count:2,
+        list:["fish","meet","chekin","drinks"]
+    };
+
+    getClasses(){
+        return this.state.count >=1 ? "btn btn-primary position-relative":"btn btn-warning position-relative";
+    };
+    add = ()=>{
+        this.setState({count: this.state.count+1});
+    };
+    render(){
+        return (
+            <div className="m-3">
+                <span className={this.getClasses()}>
+                {this.state.name}
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {this.state.count}
+                    </span>
+                </span>
+                <ul>
+                    {this.state.list.map(menu => <li key={menu}>{menu}</li>)}
+                </ul>
+                <button className='btn btn-primary' onClick={this.add}>+</button>
+            </div>
+        );
+    }
 }
- 
+    
 export default Product;
